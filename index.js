73972778b80989cf61f5a53374dea5b8cdcc9e99
Kevin Kirsche <Kev.Kirsche+GitHub@gmail.com>
2015-08-29 08:41:10 -0400

    Start working on the main code file

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-var glob = require('glob');
-var exec = require('child_process').exec;
+'use strict';
+
+var bootlint = (process.env.BOOTLINT_COV === '1') ? require('../src-cov/bootlint.js') : require('../src/bootlint.js');
 var fs = require('fs');
 var path = require('path');
 
-module.exports = FixMe;
-function FixMe() { }
+module.exports = Lint;
+function Lint() { }
+
+function lintHtml(html, disabledIds) {
+    var lints = [];
+    var reporter = function (lint) {
+        lints.push(lint.message);
+    };
+    bootlint.lintHtml(html, reporter, disabledIds || []);
+    return lints;
+}
 
 // Strings to scan for in source
-var fixmeStrings = "'(FIXME|TODO|HACK|XXX|BUG)'";
-
 var excludeExtensions = [".jpg", ".jpeg", ".png", ".gif"];
 
 // Prints properly structured Issue data to STDOUT according to
@@ -33,38 +41,6 @@ var printIssue = function(fileName, lineNum, matchedString){
   console.log(issueString);
 }
 
-var findFixmes = function(file){
-  // Prepare the grep string for execution (uses BusyBox grep)
-  var grepString = "grep -inHwoE " + fixmeStrings + " " + file;
-
-  // Execute grep with the FIXME patterns
-  exec(grepString, function (error, stdout, stderr) {
-    var results = stdout.toString();
-
-    if (results !== ""){
-      // Parses grep output
-      var lines = results.split("\n");
-      
-      lines.forEach(function(line, index, array){
-        // grep spits out an extra line that we can ignore
-        if(index < (array.length-1)){
-          // Grep output is colon delimited
-          var cols = line.split(":");
-
-          // Remove remnants of container paths for external display
-          var fileName = cols[0].split("/code/")[1];
-          var lineNum = cols[1];
-          var matchedString = cols[2];
-
-          if (matchedString !== undefined){
-            printIssue(fileName, lineNum, matchedString);
-          }
-        }
-      })
-    }
-  })
-}
-
 var eligibleFile = function(fp, excludePaths){
   return (excludePaths.indexOf(fp.split("/code/")[1]) < 0) &&
   !fs.lstatSync(fp).isDirectory() &&
@@ -86,7 +62,7 @@ var fileWalk = function(excludePaths){
   return analysisFiles;
 }
 
-FixMe.prototype.runEngine = function(){
+Lint.prototype.runEngine = function(){
   // Check for existence of config.json, parse exclude paths if it exists
   if (fs.existsSync("/config.json")) {
     var engineConfig = JSON.parse(fs.readFileSync("/config.json"));
